ee9581a205639809e23d4b2ef10509e09f470870
+ <+>
2022-11-25 22:25:23 -0700

    Logical operations

---
+++ data types/arithmetic-operators.js
@@ -0,0 +1,24 @@
+// basic operations
+console.log(12 + 7) // => 19
+console.log(44 - 13) // => 31
+console.log(9 * 5) // => 45
+console.log(8 / 5) // => 1.6
+console.log(14 % 224) // => 14
+
+console.log(2 + 3); // => 5
+console.log(42 - 42); // => 0
+console.log(-4 * 1.5); // => -6
+// advanced operations
+console.log(25 / 8); // => 3.125
+console.log(5 * 3 + 2); // => 17
+console.log(2 + 3 * 5); // => 17
+console.log((2 + 3) * 5); // => 25
+// modulo operations
+console.log(10 % 3); // => 1
+console.log(14 % 5); // => 4
+console.log(20 % 17); // => 3
+console.log(18 % 6); // => 0
+console.log(7 % 94); // => 7
+// modulo has precedence over addition
+console.log(4 + 12 % 5); // => 6
+console.log((4 + 12) % 5); // => 1
